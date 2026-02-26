c1bde3d1ffba82e0e26d94101fb1da3fbece757f
Koldkola <kenechi596@gmail.com>
2026-02-25 20:31:26 -0500

    Fix admin password hashing format to match archive (salt:password)

--- public/js/globe.js
+++ public/js/globe.js
@@ -186,7 +186,7 @@ function closeAdminPasswordModal() {
 // Hash password using SHA-256
 async function hashPassword(password, salt) {
   const encoder = new TextEncoder();
-  const data = encoder.encode(password + salt);
+  const data = encoder.encode(`${salt}:${password}`);
   const hashBuffer = await crypto.subtle.digest('SHA-256', data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
